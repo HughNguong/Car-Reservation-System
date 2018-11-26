75d030b25ccda9461c1dacfc537ebcba7327b3c0
Hugh Nguong <ductuanad2908@gmail.com>
2018-11-26 17:33:30 +0100

    CRS

---
+++ src/src/Class1.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * @author Tuan Nguyen
+ * @version 1.0
+ * @created 26-Nov-2018 5:27:39 PM
+ */
+public class Class1 {
+
+	public Class1(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+}//end Class1
